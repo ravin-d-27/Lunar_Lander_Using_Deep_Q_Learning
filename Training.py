e9aa99d1fdae843e6bd6a7168dd84d8074c2d662
Ravin D <ravin.d3107@outlook.com>
2023-11-20 18:58:59 +0530

    Set up the Hyperparameters

--- Training.py
+++ Training.py
@@ -1,3 +1,15 @@
 # Importing the Libraries
 
 import gymnasium as gym
+
+# Setting up the Lunar Environment
+
+env = gym.make('LunarLander-v2')
+stateShape = env.observation_space.shape
+print("State Shape: ",stateShape)
+
+stateSize = env.observation_space.shape[0]
+print("State Size", stateSize)
+
+numberActions = env.action_space.n
+print("Number of Actions: ", numberActions)
