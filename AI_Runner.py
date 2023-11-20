e8abe5df370baef60df9708f478386f3c1bc4541
Ravin D <ravin.d3107@outlook.com>
2023-11-20 20:51:53 +0530

    Added the Whole AI Implementation

---
+++ AI_Runner.py
@@ -0,0 +1,107 @@
+import gymnasium as gym
+from collections import deque, namedtuple
+import numpy as np
+
+from Reinforce import DQN
+
+
+# Setting up the Lunar Environment
+
+env = gym.make('LunarLander-v2')
+stateShape = env.observation_space.shape
+print("State Shape: ",stateShape)
+
+stateSize = env.observation_space.shape[0]
+print("State Size", stateSize)
+
+numberActions = env.action_space.n
+print("Number of Actions: ", numberActions)
+
+
+
+# Initializing the Hyper Parameters
+
+lr = 5e-4 # 0.0005
+miniBatchSize = 100
+gamma = 0.99
+replay_buffer_size = int(1e5) # memory of the AI
+interpolation_parameter = 1e-3 # tou
+
+
+
+### Initialize the DQN Agent
+
+agent = DQN(stateSize, numberActions)
+
+
+### Training the DQN
+
+numberEpisodes = 2000
+maxNumberTimestepsPerEpisode = 1000
+epsilonStartingValue = 1.0
+epsilonEndingValue = 0.01
+epsilonDecayValue = 0.995
+epsilon = epsilonStartingValue
+
+ScoresOn100Episodes = deque(maxlen=100)
+
+
+for episode in range(1, numberEpisodes + 1):
+  state, _ = env.reset()
+  score = 0
+  for t in range(maxNumberTimestepsPerEpisode):
+    action = agent.act(state, epsilon)
+    next_state, reward, done, _, _ = env.step(action)
+    agent.step(state, action, reward, next_state, done)
+    state = next_state
+    score += reward
+    if done:
+      break
+  ScoresOn100Episodes.append(score)
+  epsilon = max(epsilonEndingValue, epsilonDecayValue * epsilon)
+  print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(ScoresOn100Episodes)), end = "")
+  if episode % 100 == 0:
+    print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(ScoresOn100Episodes)))
+  if np.mean(ScoresOn100Episodes) >= 200.0:
+    print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode - 100, np.mean(ScoresOn100Episodes)))
+    torch.save(agent.localQNetwork.state_dict(), 'checkpoint.pth')
+    break
+
+
+
+import glob
+import io
+import base64
+import imageio
+from IPython.display import HTML, display
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+def show_video_of_model(agent, env_name):
+    env = gym.make(env_name, render_mode='rgb_array')
+    state, _ = env.reset()
+    done = False
+    frames = []
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        action = agent.act(state)
+        state, reward, done, _, _ = env.step(action.item())
+    env.close()
+    imageio.mimsave('video.mp4', frames, fps=30)
+
+show_video_of_model(agent, 'LunarLander-v2')
+
+def show_video():
+    mp4list = glob.glob('*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        display(HTML(data='''<video alt="test" autoplay
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+    else:
+        print("Could not find video")
+
+show_video()
--- Reinforce.py
+++ Reinforce.py
@@ -1,27 +1,12 @@
 # Importing the Libraries
 
-import gymnasium as gym
 import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
-
+import random
 from AI import Brain
 
-# Setting up the Lunar Environment
-
-env = gym.make('LunarLander-v2')
-stateShape = env.observation_space.shape
-print("State Shape: ",stateShape)
-
-stateSize = env.observation_space.shape[0]
-print("State Size", stateSize)
-
-numberActions = env.action_space.n
-print("Number of Actions: ", numberActions)
-
-
-# Initializing the Hyper Parameters
 
 lr = 5e-4 # 0.0005
 miniBatchSize = 100
@@ -30,13 +15,14 @@ replay_buffer_size = int(1e5) # memory of the AI
 interpolation_parameter = 1e-3 # tou
 
 
+
 # Implementing the Experience Replay
 
 class ReplayMemory(object):
 
     def __init__(self, capacity):
         
-        self.device = torch.cuda("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.capacity = capacity # Maximum size of the Memory Buffer
 
         self.memory = []
@@ -50,39 +36,20 @@ class ReplayMemory(object):
     
     def sample(self, batchSize):
         experiences = random.sample(self.memory, k = batchSize)
-
-        states = np.vstack([e[0] for e in experiences if e is not None])
-        # Converting the states into Pytorch tensors
-        states = torch.from_numpy(states).float().to(self.device) # For the designated computed device
-        
-
-        actions = np.vstack([e[1] for e in experiences if e is not None])
-        # Converting the actions into Pytorch tensors
-        actions = torch.from_numpy(actions).long().to(self.device) # For the designated computed device
-        
-        rewards = np.vstack([e[2] for e in experiences if e is not None])
-        # Converting the actions into Pytorch tensors
-        rewards = torch.from_numpy(rewards).float().to(self.device) # For the designated computed device
-
-
-        next_states = np.vstack([e[3] for e in experiences if e is not None])
-        # Converting the states into Pytorch tensors
-        next_states = torch.from_numpy(next_states).float().to(self.device) # For the designated computed device
-
-        done = np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)
-        # Converting the states into Pytorch tensors
-        done = torch.from_numpy(done).float().to(self.device) # For the designated computed device
-
-
-        return states, next_states, actions, rewards, done
-        
+        states = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+        return states, next_states, actions, rewards, dones
 
 # Implementing the Deep Q Learning Class
 
 class DQN():
 
     def __init__(self, stateSize, actionSize):
-        self.device = torch.cuda("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
         self.stateSize = stateSize
         self.actionSize = actionSize
 
@@ -116,7 +83,7 @@ class DQN():
         if random.random() > epsilon:
             return np.argmax(actionValues.cpu().data.numpy())
         else:
-            return random.choice(np.arrange(self.actionSize))
+            return random.choice(np.arange(self.actionSize))
     
     def learn(self, experiences, gamma):
         states, next_states, actions, rewards, dones = experiences
@@ -139,4 +106,5 @@ class DQN():
         for targetParam, localParam in zip(localModel.parameters(), localModel.parameters()):
             targetParam.data.copy_(tou * localParam.data + (1.0 - tou) * targetParam.data)
         
-        
+    
+
